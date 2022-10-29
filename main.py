6da8dee0198ae1b360f913485522f4da1e93975b
Erin <erin@Erins-MacBook-Air.local>
2022-10-29 15:14:47 -0700

    Add solver for optimal ratio using scipy

--- main.py
+++ main.py
@@ -10,24 +10,12 @@ yf.pdr_override()
 
 st.title('Modern Portfolio Theory')
 
-# TODO: Latex equation of CAPM
-
-# st.subheader("Part 1: CAPM (Capital Asset Pricing Model)")
-# if st.checkbox("Show"):
-
-# 	# TODO: ticker (SPY, etc) with st.metric!
-# 	mr = st.number_input("Market Return (yearly) %", 0.00, 100.00, 20.00, format="%f")
-
-# 	beta_range = np.arange(0, 2, 0.1)
-# 	return_val = beta_range * (mr - rfr) + rfr
-# 	data = np.vstack((beta_range, return_val)).T
-# 	# st.write(data)
-# 	chart_data = pd.DataFrame(
-# 	    data,
-# 	    columns=['beta', 'Return%'])
-# 	st.line_chart(chart_data, x="beta", y="Return%")
-
-
+expander = st.expander("What is Modern Portfolio Theory?")
+expander.write("""
+	Modern Portfolio Theory refers to an investment theory that allows investors to
+	assemble an asset portfolio that **maximizes** expected
+	**return** for a given level of **risk**.
+""")
 
 def user_input_features():
 	st.sidebar.caption("Pick two stocks you want to find an asset portfolio that **maximizes** expected *return* for a given level of **risk**.")
@@ -53,6 +41,8 @@ ticker1, ticker2, market_ticker, start, end,rfr = user_input_features()
 stocks = download(ticker1, ticker2, market_ticker)
 
 st.header("Closing Prices 📈")
+# TODO: format dates
+st.caption("Below shows daily closing prices for given duration from " + str(start) + " to " + str(end))
 prices = stocks["Close"]
 prices_long_form = prices.reset_index()
 prices_long_form = prices_long_form.rename(columns={'index':'Date'})
@@ -68,9 +58,10 @@ st.altair_chart(
 	.mark_line(interpolate='step-after').encode(
 		x="Date", 
 		y="price", 
-		color="ticker", 
+		color="ticker",
 		strokeDash="ticker",
 		tooltip=[
+				alt.Tooltip("ticker", title="Ticker"),
                 alt.Tooltip("Date", title="Date"),
                 alt.Tooltip("price", title="Price (USD)"),
          ]).add_selection(hover),
@@ -84,37 +75,64 @@ def summary_stat_each(ticker, data, market_return_pct, col):
 	risk_daily = daily_return.std()
 	risk_yearly = risk_daily*math.sqrt(12)
 	beta = market_return_pct
-	col.text(ticker)
-	col.text('Expected Return (daily): {:.2f}%'.format(er_daily*100))
-	col.text('Expected Return (yearly): {:.2f}%'.format(er_yearly*100))
-	col.text('Risk (daily): {:.2f}%'.format(risk_daily*100))
-	col.text('Risk (yearly): {:.2f}%'.format(risk_yearly*100))
-	return daily_return,er_yearly,risk_yearly
+	return daily_return, er_daily, er_yearly, risk_daily,risk_yearly
 
-st.header("Summary Stats 📝")
+st.header("Summary Statistics 📝")
+expander = st.expander("What does these statistics tell me?")
+expander.write("""
+	Modern Portfolio Theory employes the core idea of *diversification* (holding assets that are less correlated).
+	Diversification in a portfolio allocation strategy aims to minimize unsystematic risks (sepcific to each asset/stock).
+
+	* **Correlation** tells how correlated the two specified stock/assets are. The higher the number, the more correlated
+	* **Expected daily return** is the average(percentage diff of day N and day N + 1).
+	* **Expected yearly return** is computed by (1 + expected daily return)^252, where 252 is the number of trading days in a year.
+	* **Daily Risk** is the standard deviation of daily return.
+	* **Yearly Risk** is the standard deviation of yearly return.
+""")
 data1= prices[ticker1]
 data2= prices[ticker2]
 market_data = prices[market_ticker]
 market_return_pct = market_data.pct_change().dropna()
 col1, col2 = st.columns(2)
-daily_return1,er_yearly1,risk_daily1 = summary_stat_each(ticker1, data1, market_return_pct, col1)
-daily_return2,er_yearly2,risk_daily2 = summary_stat_each(ticker2, data2, market_return_pct, col2)
+daily_return1,er_daily1,er_yearly1,risk_daily1,risk_yearly1 = summary_stat_each(ticker1, data1, market_return_pct, col1)
+daily_return2,er_daily2,er_yearly2,risk_daily2,risk_yearly2 = summary_stat_each(ticker2, data2, market_return_pct, col2)
+df = pd.DataFrame(
+	data=np.array([
+		['{:.2f}%'.format(er_daily1*100),'{:.2f}%'.format(er_daily2*100)],
+		['{:.2f}%'.format(er_yearly1*100),'{:.2f}%'.format(er_yearly2*100)],
+		['{:.2f}%'.format(risk_daily1*100),'{:.2f}%'.format(risk_daily2*100)],
+		['{:.2f}%'.format(risk_yearly1*100),'{:.2f}%'.format(risk_yearly2*100)],
+		]),
+	columns=[ticker1, ticker2],
+	index=['Expected Return (daily)', 'Expected Return (yearly)', 'Risk (daily)', 'Risk (yearly)']
+)
 
 stacked = np.vstack((daily_return2, daily_return1)).T
 correlation_daily = np.corrcoef(daily_return2, daily_return1)[0][1]
-st.text("correlation_daily: {:.2f}".format(correlation_daily))
+st.metric(label="Correlation Factor", value="{:.2f}".format(correlation_daily))
+st.table(df)
 covariance_yearly = np.cov(daily_return2, daily_return1)*math.sqrt(252.0)
+
 # st.caption("TODO: following seem off from excel")
 # st.write(correlation_daily)
 # st.write(covariance_yearly)
-
 # col1, col2 = st.columns(2)
 # col1.metric("Correlation(daily)", '{:.2f}'.format(correlation_daily))
 # col2.metric("Covariance(yearly)", '{:.2f}'.format(covariance_yearly))
 
 st.header("\nIdeal Diversification ⚖️")
+expander = st.expander("How do I find the ideal diversification ratio between two stocks/assets?")
+expander.write("""
+	We compute two things - 1. Capital Allocation Line and 2. Efficient Portfolio
+
+	* **Capital Allocation Line** is the projected **return** & **risk** computed based on different ratio of (max return given by two assets) and (risk free rate).
+	* **Efficient Portfolio** is the projected **return** & **risk** computed based on different ratio of (asset 1) and (asset 2).
+	* **Return** of two stocks are computed by `(ratio of asset 1) * yearly return of asset 1 + (ratio of asset 2) * yearly return of asset 2` by where ratio sums up to 1.
+	* **Risk** of two stocks are computed by weight, daily expected returns, and correlation. 
+""")
+
 import matplotlib.pyplot as plt
-weights1 = np.arange(1.30, -0.3, -0.05)
+weights1 = np.arange(1.30, -0.3, -0.02)
 weights2 = 1 - weights1
 portfolio_risk = np.sqrt(weights1**2 * risk_daily1**2 + weights2**2 * risk_daily2**2 + 2 * weights1 * weights2 * correlation_daily * risk_daily1 * risk_daily2)
 portfolio_return = weights1 * er_yearly1 + weights2 * er_yearly2
@@ -140,49 +158,40 @@ cal_data = pd.DataFrame(
 
 st.caption("Optimal Ratio of " + ticker1 + ":" + ticker2 +  " is where Capital Allocation Line and Efficient Portfolio intersects in tangential fashion")
 
-st.altair_chart(alt.layer(alt.Chart(chart_data).mark_circle(size=30).encode(
+st.altair_chart(alt.layer(
+	alt.Chart(chart_data).mark_circle(size=30).encode(
 		x='Risk (Std)',
     	y='Return %',
     	tooltip=['Weight of ' + ticker1, 'Risk (Std)', 'Return %']),
-alt.Chart(cal_data).mark_line().encode(
+	alt.Chart(cal_data).mark_line().encode(
 		x='Risk (Std)',
 		y='Return %',
 		tooltip=['Weight of Optimal', 'Risk (Std)', 'Return %']
 )),
 use_container_width=True)
 
+# Solve for exact ratio
+# https://stackoverflow.com/questions/67379860/finding-intersection-of-two-functions-in-python-scipy-root-finding
+from scipy.optimize import root
+def g(x):
+	res = math.sqrt((x - er_yearly1)**2+risk_daily1**2 + (x - er_yearly2)**2+risk_daily2**2 + 2 * risk_daily1 * risk_daily2 * correlation_daily * (x - er_yearly1) * (x - er_yearly2)) / (er_yearly1 - er_yearly2) - max_risk * (x - rfr/100.0)/(max_return - rfr/100.0)
+	return res
 
-# DATE_COLUMN = 'date/time'
-# DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-#          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+sol = root(g, [0], method='hybr')
+max_return_found = sol.x
+st.metric(label="Max Return of Optimal Portfolio", value="{:.2f}%".format(max_return_found[0]* 100))
 
-# @st.cache
-# def load_data(nrows):
-#     data = pd.read_csv(DATA_URL, nrows=nrows)
-#     lowercase = lambda x: str(x).lower()
-#     data.rename(lowercase, axis='columns', inplace=True)
-#     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-#     return data
+# return of optimal = w1 * yearly return 1 + (1-w1) * yearly return 2
+# which equals to 
+# w1 = (return - yearly return 2) / (yearly return 1 - yearly return 2)
+w1 = (max_return_found - er_yearly2) / (er_yearly1 - er_yearly2)
 
-# # Create a text element and let the reader know the data is loading.
-# data_load_state = st.text('Loading data...')
-# # Load 10,000 rows of data into the dataframe.
-# data = load_data(10000)
-# # Notify the reader that the data was successfully loaded.
-# data_load_state.text("Done! (using st.cache)")
+ideal_df = pd.DataFrame(
+	data=np.array([
+		['{:.2f}%'.format(w1[0]*100),'{:.2f}%'.format((1-w1[0])*100)]
+		]),
+	columns=[ticker1, ticker2],
+	index=['Ideal Weight']
+)
 
-# if st.checkbox("Show raw data"):
-# 	st.subheader('Raw data')
-# 	st.write(data)
-
-# st.subheader('Number of pickups by hour')
-# hist_values = np.histogram(
-#     data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-
-# st.bar_chart(hist_values)
-
-# hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
-# #hour_to_filter = 17
-# filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-# st.subheader(f'Map of all pickups at {hour_to_filter}:00')
-# st.map(filtered_data)
+st.table(ideal_df)
